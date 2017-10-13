acd49bb01070e6afedf1a7d888bfe7bc567bf32d
Andres <andres@oohlalamobile.com>
2017-10-13 11:23:28 -0400

    missing files

---
+++ src/components/PostCard.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <div class="card">
+    <div class="card__thumb" v-bind:style="{ backgroundImage: 'url(' + image + ')' }"></div>
+    <div class="card__meta">
+      <router-link :to="`post/${id}`">
+        <p class="card__title">{{ title }}</p>
+      </router-link>
+      <p class="card__description">{{ body }}</p>
+
+      <div class="card__author">
+        <router-link :to="`authors/${author}`">@{{ author }}</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'post-card',
+
+  props: {
+    id: Number,
+    title: String,
+    image: String,
+    body: String,
+    author: Number,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/main.scss';
+.card {
+  border: 5px solid #fff;
+  // box-shadow: 1px 0px 8px rgba(0, 0, 0, .4);
+
+  &__thumb {
+    height: 200px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+  }
+
+  &__meta {
+    padding: 10px;
+    background: #f2f2f2;
+  }
+}
+</style>
+
---
+++ src/containers/manage/profile/ProfileList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div class="row center-xs">
+      <div class="col-xs-4 card">
+        <div class="card__image">
+          <img
+            alt="User Avatar"
+            v-bind:src="user.avatar ? user.avatar : defaultAvatar">
+        </div>
+        <div class="card__meta">
+          <ui-textbox
+            disabled
+            placeholder="First Name"
+            v-model="user.first_name">
+          </ui-textbox>
+
+          <br>
+
+          <ui-textbox
+            disabled
+            placeholder="Last Name"
+            v-model="user.last_name">
+          </ui-textbox>
+
+          <br>
+
+          <ui-textbox
+            disabled
+            placeholder="Email"
+            v-model="user.email">
+          </ui-textbox>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable global-require */
+export default {
+  name: 'profile-list',
+
+  data() {
+    return {
+      user: this.$session.g.get('user'),
+      defaultAvatar: require('@/assets/avatar/default.png'),
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../../../styles/main.scss';
+
+.card {
+  &__image {
+    width: 120px;
+    height: 120px;
+    overflow: hidden;
+    border-radius: 50%;
+    margin: 0 auto 25px auto;
+
+    img {
+      width: 120px;
+      height: 120px;
+    }
+  }
+
+  &__meta {
+    text-align: center;
+  }
+}
+</style>
+
