b2d6a200dc3ee5a6d7af22d1e2ddebda18d15bd7
Andres <andres@oohlalamobile.com>
2017-10-16 06:28:44 -0400

    latest changes

---
+++ src/containers/posts/PostEdit.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hello Post Edit
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'post-edit',
+};
+</script>
+
+<style lang="sass">
+@import '../../styles/main.scss';
+</style>
+
